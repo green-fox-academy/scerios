07fb84b7ff0133ec625bec7197bcffa5aaf5a1ad
Takács Róbert <89.t.robert@gmail.com>
2018-11-03 17:15:11 +0100

    PrintEven

---
+++ week-01/day-3/src/PrintEven.java
@@ -0,0 +1,12 @@
+public class PrintEven {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        for (i = 2; i < 500; i += 2) {
+
+            System.out.println(i);
+        }
+    }
+}
