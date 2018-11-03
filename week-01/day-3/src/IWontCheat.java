fdbf918598a68768d0ed70ef45275bc7a2d06731
Takács Róbert <89.t.robert@gmail.com>
2018-11-03 17:11:41 +0100

    IWontCheat

---
+++ week-01/day-3/src/IWontCheat.java
@@ -0,0 +1,13 @@
+public class IWontCheat {
+
+    public static void main(String[] args) {
+
+        String cheat = "I won't cheat on the exam!";
+        int i = 100;
+
+        for (i = 0; i < 100; i++) {
+
+            System.out.println(cheat);
+        }
+    }
+}
