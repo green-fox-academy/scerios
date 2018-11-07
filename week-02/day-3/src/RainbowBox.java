846a55aca96ff3939553bc2b0332f47b528ccf1b
Takács Róbert <89.t.robert@gmail.com>
2018-11-07 16:00:10 +0100

    RainbowBox

---
+++ week-02/day-3/src/RainbowBox.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class RainbowBox {
+    public static void mainDraw(Graphics graphics) {
+        int sum = (WIDTH + HEIGHT) / 2;
+        for (int i = sum; i > 0; i -= 15) {
+            int size = i;
+            rainbowSquare(graphics, size);
+        }
+    }
+    public static void rainbowSquare(Graphics graphics, int size) {
+        graphics.setColor(new Color((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255)));
+        graphics.fillRect((WIDTH / 2) - (size / 2), (HEIGHT / 2) - (size / 2), size, size);
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
