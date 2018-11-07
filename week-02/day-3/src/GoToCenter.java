49546e8a5b919f4a530ec6247e9144b15956490f
Takács Róbert <89.t.robert@gmail.com>
2018-11-07 11:29:06 +0100

    GoToCenter

---
+++ week-02/day-3/src/GoToCenter.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class GoToCenter {
+
+    public static void mainDraw(Graphics graphics) {
+        Random n = new Random();
+        Random z = new Random();
+        int low = 0;
+        int high = 320;
+
+        for (int i = 0; i < 3; i++) {
+            int result = n.nextInt(high - low) + low;
+            int result2 = z.nextInt(high - low) + low;
+            graphics.drawLine(result, result2, HEIGHT / 2, WIDTH / 2);
+
+        }
+
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
