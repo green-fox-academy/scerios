4e0378c7ad7cac89f3fbae28b8ad6e43513b5e04
Takács Róbert <89.t.robert@gmail.com>
2018-11-08 14:45:42 +0100

    ElementFinder

--- week-02/day-2/src/ElementFinder.java
+++ week-02/day-2/src/ElementFinder.java
@@ -9,9 +9,22 @@ public class ElementFinder {
             return "Noooooo";
         }
     }
+
+    public static void check(ArrayList arrayList) {
+        int[] checkList = new int[arrayList.size()];
+        for (int i = 0; i < checkList.length; i++) {
+            if (checkList[i] == 7) {
+                System.out.println("Hoorray");
+                break;
+            } else if (i == checkList.length - 1){
+                System.out.println("Noooooo");
+            }
+        }
+    }
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
 
+        check(arrayList);
         System.out.println(containsSeven(arrayList));
     }
 }
