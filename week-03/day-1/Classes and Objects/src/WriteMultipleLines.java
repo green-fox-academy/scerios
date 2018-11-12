17d81a6ace5fbb4d5d60e6153a05eaea7b6e053f
Takács Róbert <89.t.robert@gmail.com>
2018-11-12 15:35:57 +0100

    WriteMultipleLines

--- week-03/day-1/Classes and Objects/src/WriteMultipleLines.java
+++ week-03/day-1/Classes and Objects/src/WriteMultipleLines.java
@@ -9,17 +9,22 @@ import java.util.List;
 public class WriteMultipleLines {
   public static void main(String[] args) {
     writeMultiLines("to-do-list.txt", "learn, practice, rage, repeat", 5);
+
   }
 
   public static void writeMultiLines(String source, String text, int lines) {
     Path myPath = Paths.get(source);
+
     try {
       for (int i = 0; i < lines; i++) {
         List<String> addLines = Arrays.asList(text);
         Files.write(myPath, addLines, StandardOpenOption.APPEND);
+
       }
+
     } catch (IOException e) {
       return;
+
     }
   }
 }
