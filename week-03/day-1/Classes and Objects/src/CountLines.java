5870e3cddc21609e46c6b93f2e32d341ac664f77
Takács Róbert <89.t.robert@gmail.com>
2018-11-12 15:35:40 +0100

    CountLines

--- week-03/day-1/Classes and Objects/src/CountLines.java
+++ week-03/day-1/Classes and Objects/src/CountLines.java
@@ -7,21 +7,23 @@ import java.util.List;
 public class CountLines {
   public static void main(String[] args) {
     System.out.println(numLines("my-file.txt"));
+
       }
+
   public static int numLines(String source)  {
     Path file = Paths.get(source);
     List<String> lines;
     int lineNum;
+
     try {
       lines = Files.readAllLines(file);
       lineNum = lines.size();
+
     } catch (IOException e) {
       return 0;
+
     }
     return lineNum;
+
   }
-  // Write a function that takes a filename as string,
-  // then returns the number of lines the file contains.
-  // It should return zero if it can't open the file, and
-  // should not raise any error.
 }
