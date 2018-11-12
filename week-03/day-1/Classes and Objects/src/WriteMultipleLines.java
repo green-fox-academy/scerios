b45c9d4325f688936f8c5731f52795a9a3e29e71
Takács Róbert <89.t.robert@gmail.com>
2018-11-12 15:03:27 +0100

    WriteMultipleLines

---
+++ week-03/day-1/Classes and Objects/src/WriteMultipleLines.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
+
+public class WriteMultipleLines {
+  public static void main(String[] args) {
+    writeMultiLines("to-do-list.txt", "learn, practice, rage, repeat", 5);
+  }
+
+  public static void writeMultiLines(String source, String text, int lines) {
+    Path myPath = Paths.get(source);
+    try {
+      for (int i = 0; i < lines; i++) {
+        List<String> addLines = Arrays.asList(text);
+        Files.write(myPath, addLines, StandardOpenOption.APPEND);
+      }
+    } catch (IOException e) {
+      return;
+    }
+  }
+}
