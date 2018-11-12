894a4036e2817cbcfce1a9f6f4c6fec8ffd87629
Takács Róbert <89.t.robert@gmail.com>
2018-11-12 15:36:14 +0100

    WriteSingleLine

--- week-03/day-1/Classes and Objects/src/WriteSingleLine.java
+++ week-03/day-1/Classes and Objects/src/WriteSingleLine.java
@@ -9,11 +9,14 @@ import java.util.List;
 public class WriteSingleLine {
   public static void main(String[] args) {
     Path source = Paths.get("my-file.txt");
+
     try {
       List<String> lines = Arrays.asList("Takács Róbert");
       Files.write(source, lines, Charset.forName("UTF-8"));
+
     } catch (IOException e) {
       System.out.println("Unable to write file: my-file.txt");
+
     }
   }
 }
