3d614deed0336fd642813b9cd54457ac3fdff429
Takács Róbert <89.t.robert@gmail.com>
2018-11-12 11:56:16 +0100

    DivideByZero

---
+++ week-03/day-1/Classes and Objects/src/DivideByZero.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DivideByZero {
+
+  public static void main(String[] args) {
+    getNum();
+
+  }
+  public static void getNum() {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a number to divide 10 with it:");
+    int x = scan.nextInt();
+    int div = 10;
+    try {
+      int result = div / x;
+      System.out.println(result);
+    } catch (ArithmeticException e) {
+      System.out.println("Fail");
+    }
+  }
+}
