0f4bbeea11e481144832d425929b6938ca01c43b
Takács Róbert <89.t.robert@gmail.com>
2018-11-12 15:35:25 +0100

    PrintEachLine

--- week-03/day-1/Classes and Objects/src/PrintEachLine.java
+++ week-03/day-1/Classes and Objects/src/PrintEachLine.java
@@ -6,12 +6,16 @@ import java.nio.file.Files;
 public class PrintEachLine {
   public static void main(String[] args) {
     Path source = Paths.get("my-file.txt");
+
     try {
         for (String line : Files.readAllLines(source)) {
         System.out.println(line);
+
       }
+
     } catch (IOException e) {
       System.out.println("Unable to read file: my-file.txt");
+
     }
   }
 }
