0014d0fff7c1043d56e650384836785c9eb6f147
Takács Róbert <89.t.robert@gmail.com>
2018-11-13 13:04:50 +0100

    Animals

---
+++ week-03/day-2/src/Animals/Animals.java
@@ -0,0 +1,26 @@
+package Animals;
+
+public class Animals {
+  int hunger;
+  int thirst;
+
+  public Animals() {
+    this.hunger = 50;
+    this.thirst = 50;
+  }
+
+  public void eat() {
+    this.hunger--;
+  }
+  public void drink() {
+    this.thirst--;
+  }
+  public void play() {
+    this.hunger++;
+    this.thirst++;
+  }
+  public void stat() {
+    System.out.println("Hunger: " + hunger);
+    System.out.println("Thirst: " + thirst);
+  }
+}
---
+++ week-03/day-2/src/Animals/Behavior.java
@@ -0,0 +1,21 @@
+package Animals;
+
+public class Behavior {
+  public static void main(String[] args) {
+
+    Animals pig = new Animals();
+    Animals lion = new Animals();
+
+    pig.eat();
+    pig.play();
+    pig.eat();
+    pig.eat();
+    pig.stat();
+    lion.play();
+    lion.play();
+    lion.play();
+    lion.drink();
+    lion.stat();
+
+  }
+}
