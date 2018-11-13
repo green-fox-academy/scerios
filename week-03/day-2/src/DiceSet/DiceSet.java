785742062cb923e7ef3b1466f3bb814af2777de1
Takács Róbert <89.t.robert@gmail.com>
2018-11-13 17:25:15 +0100

    DiceSet

---
+++ week-03/day-2/src/DiceSet/DiceSet.java
@@ -0,0 +1,46 @@
+package DiceSet;
+
+import java.util.Arrays;
+
+public class DiceSet {
+
+  int[] dices = new int[6];
+
+  public int[] roll() {
+    for (int i = 0; i < dices.length; i++) {
+      dices[i] = (int) (Math.random() * 6) + 1;
+    }
+    return dices;
+  }
+
+  public int[] getCurrent() {
+    return dices;
+  }
+
+  public int getCurrent(int i) {
+    return dices[i];
+  }
+
+  public void reroll() {
+    for (int i = 0; i < dices.length; i++) {
+      dices[i] = (int) (Math.random() * 6) + 1;
+    }
+  }
+
+  public void reroll(int k) {
+    dices[k] = (int) (Math.random() * 6) + 1;
+  }
+
+  public static void main(String[] args) {
+    DiceSet diceSet = new DiceSet();
+    diceSet.roll();
+    for (int i = 0; i < 6; i++) {
+      do {
+        diceSet.reroll(i);
+      } while (diceSet.dices[i] != 6);
+
+      System.out.print(diceSet.dices[i] + " ");
+    }
+  }
+}
+
