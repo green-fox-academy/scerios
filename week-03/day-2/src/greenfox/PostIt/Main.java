5316016ba45116c5b1f8398a188092f1148e0e97
Takács Róbert <89.t.robert@gmail.com>
2018-11-13 11:45:32 +0100

    PostIt

---
+++ week-03/day-2/src/greenfox/PostIt/Main.java
@@ -0,0 +1,10 @@
+package greenfox.PostIt;
+
+public class Main {
+
+  public static void main(String[] args) {
+    PostIt one = new PostIt("orange", "Idea-1", "blue");
+    PostIt two = new PostIt("pink", "Awesome", "black");
+    PostIt three = new PostIt("yellow", "Superb!", "green");
+    }
+}
---
+++ week-03/day-2/src/greenfox/PostIt/PostIt.java
@@ -0,0 +1,13 @@
+package greenfox.PostIt;
+
+public class PostIt {
+  String background;
+  String text;
+  String textColor;
+
+  public PostIt(String background, String text, String textColor) {
+    this.background = background;
+    this.text = text;
+    this.textColor = textColor;
+  }
+}
