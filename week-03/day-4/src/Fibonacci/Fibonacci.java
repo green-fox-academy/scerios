6cc9f0183606cbe61e1844e3ad4074ad82ceb281
Takács Róbert <89.t.robert@gmail.com>
2018-11-15 17:40:57 +0100

    Fibonacci

---
+++ week-03/day-4/src/Fibonacci/Fibonacci.java
@@ -0,0 +1,19 @@
+package Fibonacci;
+
+public class Fibonacci {
+  public static void main(String[] args) {
+
+    System.out.println(fibonacci(7));
+  }
+
+  private static long fibonacci(int i) {
+    if (i == 0) {
+      return 0;
+    } else if (i <= 2) {
+      return 1;
+    }
+
+    long fib = fibonacci(i - 1) + fibonacci(i - 2);
+    return fib;
+  }
+}
