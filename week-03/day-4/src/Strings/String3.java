d460b6bc79e8aa0845bd92d12f17874526098656
Takács Róbert <89.t.robert@gmail.com>
2018-11-15 17:10:18 +0100

    String3 finished

--- week-03/day-4/src/Strings/String3.java
+++ week-03/day-4/src/Strings/String3.java
@@ -2,15 +2,15 @@ package Strings;
 
 public class String3 {
   public static void main(String[] args) {
-    System.out.println(removeLetter("xrexcuxxrsion xixs stxxuxpixd"));
+    System.out.println(insertStar("sup mate"));
 
   }
-  public static String removeLetter(String word) {
-    if (word.length() < 1) {
-      return word;
+  public static String insertStar(String star) {
+    if(star.length() == 1) {
+      return star;
     } else {
-      String remove = word.startsWith("") ? "*" : word.substring(0, 1);
-      return remove + removeLetter(word.substring(1));
+      String subString = star.substring(1);
+      return star.substring(0,1) + "*" + insertStar(subString);
     }
   }
 }
