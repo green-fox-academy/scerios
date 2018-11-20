d07ccb5b506630a9f07a091caf255af911687800
Takács Róbert <89.t.robert@gmail.com>
2018-11-20 10:20:33 +0100

    I didn't do anything. Yesterday's trial exam had some strange happenings.

---
+++ week-03/TrialExam/src/trialexam/Seconds.java
@@ -0,0 +1,22 @@
+package trialexam;
+
+import java.util.ArrayList;
+
+public class Seconds {
+
+  public static void main(String[] args) {
+
+    // Create a function that takes a list as a parameter,
+    // and returns a new list with every second element from the original list
+    // example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
+  }
+  public static ArrayList<Integer> onlyTheSecond(ArrayList<Integer> numList) {
+
+    for (int i = 0; i < numList.size(); i +=2) {
+
+    }
+    return numList;
+  }
+}
+
+
