c5291ca26746b990d9221d1e96b7e39b703d1aa9
Takács Róbert <89.t.robert@gmail.com>
2018-11-21 15:52:33 +0100

    Typo corrected in 'App' main class

--- week-04/day-3/src/App.java
+++ week-04/day-3/src/App.java
@@ -12,7 +12,7 @@ public class App {
     bassGuitar.play();
     violin.play();
 
-    System.out.println("Test 2, create Electric Guitar, Bass Guitar with 7 and 5 strings .");
+    System.out.println("Test 2, create Electric Guitar, Bass Guitar with 7 and 5 strings.");
     ElectricGuitar guitar2 = new ElectricGuitar(7);
     BassGuitar bassGuitar2 = new BassGuitar(5);
 
