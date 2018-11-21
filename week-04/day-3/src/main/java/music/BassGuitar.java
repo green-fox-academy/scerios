94c6c6662e027d162f5d8dd7603f090c11a6f26b
Takács Róbert <89.t.robert@gmail.com>
2018-11-21 14:56:49 +0100

    Created 'BassGuitar' abstract class

---
+++ week-04/day-3/src/main/java/music/BassGuitar.java
@@ -0,0 +1,13 @@
+package main.java.music;
+
+public class BassGuitar extends StringedInstrument {
+  BassGuitar() {
+    super.name = "Duum-duum-duum";
+    super.numberOfStrings = 4;
+  }
+
+  BassGuitar(int numberOfStrings) {
+    super.name = "Duum-duum-duum";
+    super.numberOfStrings = numberOfStrings;
+  }
+}
