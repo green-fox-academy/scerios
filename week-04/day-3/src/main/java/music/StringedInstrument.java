d8f1fd618591936fe8ff2fcfce2a51c57e9afd56
Takács Róbert <89.t.robert@gmail.com>
2018-11-21 15:53:34 +0100

    Reshaped methods, now working properly

--- week-04/day-3/src/main/java/music/StringedInstrument.java
+++ week-04/day-3/src/main/java/music/StringedInstrument.java
@@ -3,7 +3,10 @@ package main.java.music;
 public abstract class StringedInstrument extends Instrument {
   protected int numberOfStrings;
 
-  void sound() {
-
+  public void play() {
+    System.out.println(getClass().getSimpleName() + ", a " + this.numberOfStrings + "-stringed instrument that goes " +
+        this.sound());
   }
+
+  abstract String sound();
 }
