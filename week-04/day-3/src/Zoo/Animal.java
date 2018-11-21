8c1cf857261144ce81429db9b8796011dd51b13e
Takács Róbert <89.t.robert@gmail.com>
2018-11-21 16:44:30 +0100

    Created Animal class for 'Zoo'

---
+++ week-04/day-3/src/Zoo/Animal.java
@@ -0,0 +1,23 @@
+package Zoo;
+
+public abstract class Animal {
+  String name;
+  String gender;
+  String feedingHabitat;
+  int age;
+  int weightInKG;
+
+  public String getName() {
+    return name;
+  }
+
+  public String breed() {
+    return "";
+  }
+
+  void eat() {
+  }
+
+  void sleep() {
+  }
+}
