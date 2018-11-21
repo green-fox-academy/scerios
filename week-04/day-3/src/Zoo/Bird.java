fad2311608d8316f6a237361d97c0d70e59b8443
Takács Róbert <89.t.robert@gmail.com>
2018-11-21 17:27:46 +0100

    Implemented 'Flyable' in 'Bird' class for Fly exercise

--- week-04/day-3/src/Zoo/Bird.java
+++ week-04/day-3/src/Zoo/Bird.java
@@ -1,6 +1,8 @@
 package Zoo;
 
-public class Bird extends Animal {
+import Fly.Flyable;
+
+public class Bird extends Animal implements Flyable {
   public Bird(String name) {
     super.name = name;
   }
@@ -13,5 +15,20 @@ public class Bird extends Animal {
   @Override
   public String breed() {
     return "laying eggs.";
+  }
+
+  @Override
+  public void takeOff() {
+    System.out.println("The " + name + " takes off.");
+  }
+
+  @Override
+  public void fly() {
+    System.out.println("The " + name + " flies.");
+  }
+
+  @Override
+  public void land() {
+    System.out.println("The " + name + " lands.");
   }
 }
