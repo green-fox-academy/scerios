12b8a02b6f898fd0e355063406b0fb978cf69787
Takács Róbert <89.t.robert@gmail.com>
2018-11-23 10:17:37 +0100

    Created 'Apple' class for test.

---
+++ week-04/day-4/src/tests/Apple.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class Apple {
+  private String name;
+  public Apple(String name) {
+    this.name = name;
+  }
+
+  public String getApple() {
+    return this.name;
+  }
+}
