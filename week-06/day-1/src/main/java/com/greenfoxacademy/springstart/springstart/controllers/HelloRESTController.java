79b6f7e7a22a31646e2a2aea8a0ee3b25eedd26a
Takács Róbert <89.t.robert@gmail.com>
2018-12-05 15:02:19 +0100

    AtomicLong class implemented and working as intended

--- week-06/day-1/src/main/java/com/greenfoxacademy/springstart/springstart/controllers/HelloRESTController.java
+++ week-06/day-1/src/main/java/com/greenfoxacademy/springstart/springstart/controllers/HelloRESTController.java
@@ -5,12 +5,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
+  AtomicLong counter = new AtomicLong(1);
 
   @RequestMapping(value="/greetings")
   @ResponseBody
   public Greeting greeting(@RequestParam("name") String name) {
-    return new Greeting(1, "Yarr " + name);
+    return new Greeting(counter.getAndIncrement(), "Yarr " + name);
   }
 }
