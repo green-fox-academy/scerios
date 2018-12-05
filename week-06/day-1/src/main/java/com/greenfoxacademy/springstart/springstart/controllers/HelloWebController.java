07e322b81afe4c779ce5e0820b2661615d0aeb5b
Takács Róbert <89.t.robert@gmail.com>
2018-12-05 16:41:24 +0100

    Implemented counter, changed the text field, working as intended

--- week-06/day-1/src/main/java/com/greenfoxacademy/springstart/springstart/controllers/HelloWebController.java
+++ week-06/day-1/src/main/java/com/greenfoxacademy/springstart/springstart/controllers/HelloWebController.java
@@ -3,13 +3,18 @@ package com.greenfoxacademy.springstart.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
+  AtomicLong count = new AtomicLong(1);
 
   @RequestMapping("/web/greetings")
-  public String greetings(Model model) {
-    model.addAttribute("name", " World");
+  public String greetings(@RequestParam("name")String name, Model model) {
+    model.addAttribute("id", count.getAndIncrement());
+    model.addAttribute("name", name);
     return "greetings";
   }
 }
