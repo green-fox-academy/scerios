83df435087217c903d23674219023bf18ac3a276
Takács Róbert <89.t.robert@gmail.com>
2018-12-05 15:26:09 +0100

    @ResponseBody added back

--- week-06/day-1/src/main/java/com/greenfoxacademy/springstart/springstart/Hello.java
+++ week-06/day-1/src/main/java/com/greenfoxacademy/springstart/springstart/Hello.java
@@ -2,11 +2,13 @@ package com.greenfoxacademy.springstart.springstart;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Hello {
 
   @RequestMapping("/hello")
+  @ResponseBody
   public String hello() {
     return "Yarr matey!";
   }
