76fe7cf2e5b66361339d2e09e1ec87ec85327206
Takács Róbert <89.t.robert@gmail.com>
2018-12-07 15:45:14 +0100

    .gitignore not working properly thus the many thrash file

--- week-06/day-3/src/main/java/com/webshop/Controllers/WebshopController.java
+++ week-06/day-3/src/main/java/com/webshop/Controllers/WebshopController.java
@@ -8,9 +8,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class WebshopController {
 
-  @RequestMapping(value = "/addItem", method = RequestMethod.GET)
-  public String items(Model model) {
+  @RequestMapping(value = "/getItem", method = RequestMethod.GET)
+  public String getitems(Model model) {
     model.addAttribute("item", Stock.listOfItems);
     return "webshop-index";
   }
+
+//  @RequestMapping(value = "/addItem", method = RequestMethod.POST)
+//  public String additems(Model model) {
+//    model.addAttribute("item", Stock.listOfItems);
+//    return "webshop-index";
+//  }
 }
