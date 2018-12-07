f332dd6c8ce6488491ca4bba3a85d37326bef8b5
Takács Róbert <89.t.robert@gmail.com>
2018-12-07 17:56:38 +0100

    Listing works, adding still not

--- week-06/day-3/src/main/java/com/webshop/Controllers/WebshopController.java
+++ week-06/day-3/src/main/java/com/webshop/Controllers/WebshopController.java
@@ -1,22 +1,32 @@
 package com.webshop.Controllers;
 
+import com.webshop.Item;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WebshopController {
 
-  @RequestMapping(value = "/getItem", method = RequestMethod.GET)
+  @RequestMapping(value = "/", method = RequestMethod.GET)
   public String getitems(Model model) {
     model.addAttribute("item", Stock.listOfItems);
     return "webshop-index";
   }
 
-//  @RequestMapping(value = "/addItem", method = RequestMethod.POST)
-//  public String additems(Model model) {
-//    model.addAttribute("item", Stock.listOfItems);
-//    return "webshop-index";
-//  }
+  @RequestMapping(value = "/addItem", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+  public String addItem(@RequestBody MultiValueMap<String, String> formData) {
+    Item newItem = new Item(
+        formData.toSingleValueMap().get("name"),
+        formData.toSingleValueMap().get("description"),
+        Double.parseDouble(formData.toSingleValueMap().get("price")),
+        Integer.parseInt(formData.toSingleValueMap().get("quantity"))
+    );
+    Stock.listOfItems.add(newItem);
+    return "webshop-index";
+  }
 }
