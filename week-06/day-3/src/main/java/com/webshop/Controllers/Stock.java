19a9393599ad971877b6f63db29f86679b1d8f63
Takács Róbert <89.t.robert@gmail.com>
2018-12-11 23:19:54 +0100

    Removed outcommented lines, they were just for testing

--- week-06/day-3/src/main/java/com/webshop/Controllers/Stock.java
+++ week-06/day-3/src/main/java/com/webshop/Controllers/Stock.java
@@ -7,10 +7,6 @@ public class Stock {
   public static ArrayList<Item> listOfItems = new ArrayList<>();
 
   public Stock() {
-//    listOfItems.add(new Item("FootBall", "NFL approved", 75, 12));
-//    listOfItems.add(new Item("Helmet (Limited Edition)", "With your favourite team's logo", 199.99, 3));
-//    listOfItems.add(new Item("Training short", "Under Armour", 44.9, 20));
-//    listOfItems.add(new Item("Nike shoes", "Built for running", 179.99, 7));
   }
 
   public ArrayList<Item> getListOfItems() {
