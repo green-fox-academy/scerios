faaf6efe4f7aa0150a57857440e86172c82311ab
Takács Róbert <89.t.robert@gmail.com>
2018-12-11 23:18:30 +0100

    Removed local arraylist as it was not in use anymore

--- week-06/day-3/src/main/java/com/webshop/WebshopApplication.java
+++ week-06/day-3/src/main/java/com/webshop/WebshopApplication.java
@@ -1,6 +1,5 @@
 package com.webshop;
 
-import com.webshop.Controllers.Stock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,15 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebshopApplication {
 
   public static void main(String[] args) {
-    Stock stock = new Stock();
-    Item football = new Item("FootBall", "NFL approved", 75, 12);
-    Item helmet = new Item("Helmet (Limited Edition)", "With your favourite team's logo", 199.99, 3);
-    Item trainingShort = new Item("Training short", "Under Armour", 44.9, 20);
-    Item nikeShoes = new Item("Nike shoes", "Built for running", 179.99, 7);
-    stock.addItems(football);
-    stock.addItems(helmet);
-    stock.addItems(trainingShort);
-    stock.addItems(nikeShoes);
     SpringApplication.run(WebshopApplication.class, args);
   }
 }
