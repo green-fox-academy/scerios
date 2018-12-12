e6adc964971bb0de9e03f956e7250cbe8d7dc6ca
Takács Róbert <89.t.robert@gmail.com>
2018-12-12 12:42:35 +0100

    Starting again...

---
+++ week-06/day-6/src/main/java/thymeleaf/bank/BankOfAnimalsApplication.java
@@ -0,0 +1,12 @@
+package thymeleaf.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOfAnimalsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BankOfAnimalsApplication.class, args);
+  }
+}
