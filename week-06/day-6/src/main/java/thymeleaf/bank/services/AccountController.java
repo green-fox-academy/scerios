e376deb23fccab97e045aee47ffae10f22d9dd5e
Takács Róbert <89.t.robert@gmail.com>
2018-12-12 13:41:02 +0100

    Add new Account and list Account(s) created. Whole class redesigned.

--- week-06/day-6/src/main/java/thymeleaf/bank/services/AccountController.java
+++ week-06/day-6/src/main/java/thymeleaf/bank/services/AccountController.java
@@ -1,10 +1,35 @@
 package thymeleaf.bank.services;
 
+import thymeleaf.bank.repository.AccountsDatabase;
 import thymeleaf.bank.repository.BankAccount;
+
+import java.util.ArrayList;
 
 public class AccountController {
 
   public void addAccount(BankAccount account) {
+    boolean isThereAccount = false;
+    for (BankAccount bankAccount : AccountsDatabase.getInstance().getListOfAccounts()) {
+      if (bankAccount.getName() == account.getName()) {
+        isThereAccount = true;
+        bankAccount.balance += account.balance;
+      }
+    }
+    if (!isThereAccount) {
+      AccountsDatabase.getInstance().addAccount(account);
+    }
+  }
 
+  public ArrayList<BankAccount> getBankAccounts() {
+    return AccountsDatabase.getInstance().getListOfAccounts();
+  }
+
+  public BankAccount getBankAccount(String name) {
+    for (BankAccount bankAccount : AccountsDatabase.getInstance().getListOfAccounts()) {
+      if (bankAccount.getName() == name) {
+        return bankAccount;
+      }
+    }
+    return null;
   }
 }
