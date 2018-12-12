9f34f45f3e752726f0d054b27b70eb10d3edb5fb
Takács Róbert <89.t.robert@gmail.com>
2018-12-12 12:23:43 +0100

    Basic request handler created with onlye GET mapping

---
+++ week-06/day-6/src/main/java/bank/of/animals/controllers/RequestHandler.java
@@ -0,0 +1,15 @@
+package bank.of.animals.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RequestHandler {
+
+  @RequestMapping("/show")
+  public String showDetails(Model model) {
+    model.addAttribute("account", AccountController.listOfAccounts);
+    return "show";
+  }
+}
