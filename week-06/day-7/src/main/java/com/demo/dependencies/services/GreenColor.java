7a03848cfdff5633d6c17e8d4fc43ed251525175
Takács Róbert <89.t.robert@gmail.com>
2018-12-13 14:53:16 +0100

    Service annotation added

--- week-06/day-7/src/main/java/com/demo/dependencies/services/GreenColor.java
+++ week-06/day-7/src/main/java/com/demo/dependencies/services/GreenColor.java
@@ -1,7 +1,9 @@
 package com.demo.dependencies.services;
 
 import com.demo.dependencies.interfaces.Coloring;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GreenColor implements Coloring {
   @Override
   public void printColor(String color) {
