30c1e07b34341431db1afb786d6b4592e2617120
Takács Róbert <89.t.robert@gmail.com>
2018-12-13 12:07:31 +0100

    New basic project created for dependencies workshop

---
+++ week-06/day-7/src/main/java/com/demo/dependencies/DependenciesApplication.java
@@ -0,0 +1,14 @@
+package com.demo.dependencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependenciesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DependenciesApplication.class, args);
+  }
+
+}
+
