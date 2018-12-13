c78225dc1f587f8cbd01f1a7516deda812164be5
Takács Róbert <89.t.robert@gmail.com>
2018-12-13 14:35:32 +0100

    color services added

---
+++ week-06/day-7/src/main/java/com/demo/dependencies/services/GreenColor.java
@@ -0,0 +1,10 @@
+package com.demo.dependencies.services;
+
+import com.demo.dependencies.interfaces.Coloring;
+
+public class GreenColor implements Coloring {
+  @Override
+  public void printColor() {
+
+  }
+}
---
+++ week-06/day-7/src/main/java/com/demo/dependencies/services/RedColor.java
@@ -0,0 +1,10 @@
+package com.demo.dependencies.services;
+
+import com.demo.dependencies.interfaces.Coloring;
+
+public class RedColor implements Coloring {
+  @Override
+  public void printColor() {
+
+  }
+}
