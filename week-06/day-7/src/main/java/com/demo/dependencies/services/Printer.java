69506ce3631dedd70a0f0eabc7da41e1a8595bb1
Takács Róbert <89.t.robert@gmail.com>
2018-12-13 14:49:32 +0100

    Separating messages added

--- week-06/day-7/src/main/java/com/demo/dependencies/services/Printer.java
+++ week-06/day-7/src/main/java/com/demo/dependencies/services/Printer.java
@@ -1,13 +1,13 @@
-package com.demo.dependencies.controller;
+package com.demo.dependencies.services;
 
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 
 @Service
-public class Printer implements CommandLineRunner {
-
-  @Override
+public class Printer {
   public void log(String message) {
+    System.out.println("------ MESSAGE ------");
     System.out.println(LocalDateTime.now() + " My printer says: " + message);
+    System.out.println("------ MESSAGE ------");
   }
 }
