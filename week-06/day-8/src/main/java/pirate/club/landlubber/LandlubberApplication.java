e11f16010369f2fb2134ee0f79852bee171fb013
Takács Róbert <89.t.robert@gmail.com>
2018-12-14 10:01:05 +0100

    Basic project structure created for Pirates Club

---
+++ week-06/day-8/src/main/java/pirate/club/landlubber/LandlubberApplication.java
@@ -0,0 +1,14 @@
+package pirate.club.landlubber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandlubberApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LandlubberApplication.class, args);
+  }
+
+}
+
---
+++ week-06/day-8/src/test/java/pirate/club/landlubber/LandlubberApplicationTests.java
@@ -0,0 +1,17 @@
+package pirate.club.landlubber;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LandlubberApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
