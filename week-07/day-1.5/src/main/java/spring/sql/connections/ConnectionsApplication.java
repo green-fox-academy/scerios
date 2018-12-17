9116aa6dcd4d0639dad2434cf5c2bd5891c4f6b1
Takács Róbert <89.t.robert@gmail.com>
2018-12-17 14:11:17 +0100

    Basic project created for exercises

---
+++ week-07/day-1.5/src/main/java/spring/sql/connections/ConnectionsApplication.java
@@ -0,0 +1,14 @@
+package spring.sql.connections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConnectionsApplication.class, args);
+  }
+
+}
+
