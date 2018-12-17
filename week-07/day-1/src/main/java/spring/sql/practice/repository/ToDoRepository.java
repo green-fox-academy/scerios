009515186fc4130cb7822d3d3a239e431d01093f
Takács Róbert <89.t.robert@gmail.com>
2018-12-17 12:32:33 +0100

    ToDoRepo.. defined as @Repository

--- week-07/day-1/src/main/java/spring/sql/practice/repository/ToDoRepository.java
+++ week-07/day-1/src/main/java/spring/sql/practice/repository/ToDoRepository.java
@@ -1,6 +1,8 @@
 package spring.sql.practice.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ToDoRepository extends CrudRepository<ToDo, Long> {
 }
