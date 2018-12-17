5cdb11ca3fbeb22bdc826cfee66261d203b744c9
Takács Róbert <89.t.robert@gmail.com>
2018-12-17 14:04:07 +0100

    samples added for test purpose, everything's good

--- week-07/day-1/src/main/java/spring/sql/practice/PracticeApplication.java
+++ week-07/day-1/src/main/java/spring/sql/practice/PracticeApplication.java
@@ -1,14 +1,23 @@
 package spring.sql.practice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import spring.sql.practice.controller.ToDoController;
+import spring.sql.practice.repository.ToDo;
 
 @SpringBootApplication
-public class PracticeApplication {
+public class PracticeApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(PracticeApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+    ToDoController.services.addToDo(new ToDo("Task 1"));
+    ToDoController.services.addToDo(new ToDo("Task 2"));
+    ToDoController.services.addToDo(new ToDo("Task 3"));
+  }
 }
 
