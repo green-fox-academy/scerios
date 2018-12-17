86b498c8e283ca097a52c91dd33d2c4f6a6a08ad
Takács Róbert <89.t.robert@gmail.com>
2018-12-17 11:41:54 +0100

    Getters added, also ID and Generatedvalue clarified

--- week-07/day-1/src/main/java/spring/sql/practice/repository/ToDo.java
+++ week-07/day-1/src/main/java/spring/sql/practice/repository/ToDo.java
@@ -1,9 +1,14 @@
 package spring.sql.practice.repository;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class ToDo {
+
+  @Id
+  @GeneratedValue
   private long ID;
   private String title;
   private boolean isUrgent;
@@ -14,5 +19,21 @@ public class ToDo {
     this.title = title;
     this.isUrgent = false;
     this.isDone = false;
+  }
+
+  public long getID() {
+    return ID;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public boolean isUrgent() {
+    return isUrgent;
+  }
+
+  public boolean isDone() {
+    return isDone;
   }
 }
