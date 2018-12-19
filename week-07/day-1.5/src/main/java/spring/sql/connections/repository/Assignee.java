91fb7678178ad9a7b5eff682ad44099282da0998
Takács Róbert <89.t.robert@gmail.com>
2018-12-19 15:08:06 +0100

    Added annotations to Assignee

--- week-07/day-1.5/src/main/java/spring/sql/connections/repository/Assignee.java
+++ week-07/day-1.5/src/main/java/spring/sql/connections/repository/Assignee.java
@@ -1,6 +1,15 @@
 package spring.sql.connections.repository;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Assignee {
+
+  @Id
+  @GeneratedValue
+  private long id;
   private String name;
   private String email;
 
